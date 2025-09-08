36c1b2b6ec783be7d8f8d66595a08914d52cd282
gustavonobrega <gustavo7nobrega@gmail.com>
2025-09-08 15:34:11 -0300

    enhance game to store input guesses

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import GuessInput from "../GuessInput";
 import GuessResults from "../GuessResults";
 
 import "./styles.css";
 
 export default function Game() {
+  const [guesses, setGuesses] = useState<string[]>([]);
+
+   function handleSubmitGuess(tentativeGuess: string) {
+    const nextGuesses = [...guesses, tentativeGuess];
+    setGuesses(nextGuesses);
+  }
+
   return (
     <>
-      <GuessResults />
+      <GuessResults  />
 
-      <GuessInput />
+      <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
   );
 }
--- src/components/GuessInput/index.tsx
+++ src/components/GuessInput/index.tsx
@@ -1,21 +1,36 @@
-import { useState } from "react";
+import { useState, type FormEvent } from "react";
 
 import "./styles.css";
 
-function GuessInput() {
-  const [tentativeGuess, setTentativeGuess] = useState('');
+interface GuessInputProps {
+  handleSubmitGuess: (tentativeGuess: string) => void;
+}
+
+function GuessInput({ handleSubmitGuess }: GuessInputProps) {
+  const [tentativeGuess, setTentativeGuess] = useState("");
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    handleSubmitGuess(tentativeGuess);
+
+    setTentativeGuess("");
+  }
 
   return (
-    <form className="guess-input-wrapper">
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
       <input
         id="guess-input"
         type="text"
         value={tentativeGuess}
-        onChange={(event) => setTentativeGuess(event.target.value.toLocaleUpperCase())}
+        onChange={(event) =>
+          setTentativeGuess(event.target.value.toLocaleUpperCase())
+        }
         required
         minLength={5}
         maxLength={5}
+        title="5 letter word"
       />
     </form>
   );
